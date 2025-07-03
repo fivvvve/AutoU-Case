e988c5f1755ab8b746e562c2de1bffcfb71bdec9
Thiago Fernandes <86969851+fivvvve@users.noreply.github.com>
2025-07-03 17:31:17 -0300

    update error responses

--- backend/routes/classify_email.py
+++ backend/routes/classify_email.py
@@ -5,27 +5,29 @@ from utils.extract_content_from_file import extract_text_from_pdf_bytes, extract
 from utils.nlp_processor import preprocess_text
 from utils.ai_service import classify_email_async
 
-
 router = APIRouter(prefix="/email", tags=["Classify Email"])
 
 # classify and genererante email response
 async def classify_email(email_content: str):
     if not email_content or not email_content.strip():
-        raise HTTPException(
-            status_code=422,
-            detail="O conteúdo do email não pode ser vazio"
-        )
+        raise HTTPException(status_code=422, detail={
+            "ok": 1,
+            "message": "O conteúdo do email não pode ser vazio"
+        })
 
     processed_text = preprocess_text(email_content)
     response = await classify_email_async(processed_text)
 
     if "Erro" in response["classification"] or "Indefinido" in response["classification"]:
-        raise HTTPException(status_code=500, detail=f"Não foi possível classificar o email fornecido")
+        raise HTTPException(status_code=500, detail={
+            "ok": 1,
+            "message": "Não foi possível classificar o email fornecido"
+        })
     
     return {
         "original_content": email_content,
         "classification": response["classification"],
-        "suggested_response": response["suggested_response"]
+        "suggested_response": response["suggested_response"] if response["suggested_response"] else "Nenhuma resposta necessária."
     }
 
 
@@ -38,6 +40,7 @@ async def classify_from_text(payload: EmailTextPayload):
     """
     Receives an email text content and classify it
     """
+
     return await classify_email(payload.text)
 
 
@@ -52,24 +55,33 @@ async def classify_from_file(email_file: Annotated[UploadFile, File()]):
     file_bytes = await email_file.read()
 
     if not email_file.filename:
-         raise HTTPException(status_code=400, detail="Nome de arquivo inválido")
+         raise HTTPException(status_code=400, detail={
+            "ok": 1,
+            "message": "Nome de arquivo inválido"
+        })
 
     # verify if it is an pdf or txt file
     if email_file.filename.lower().endswith(".pdf"):
         try:
             email_content = extract_text_from_pdf_bytes(file_bytes)
         except Exception as e:
-            raise HTTPException(status_code=500, detail=f"Erro ao processar PDF")
+            raise HTTPException(status_code=500, detail={
+                "ok": 1,
+                "message": "Erro ao processar PDF"
+            })
     
     elif email_file.filename.lower().endswith(".txt"):
         try:
             email_content = extract_text_from_txt_bytes(file_bytes)
         except Exception as e:
-            raise HTTPException(status_code=500, detail=f"Erro ao processar TXT")
+            raise HTTPException(status_code=500, detail={
+                "ok": 1,
+                "message": "Erro ao processar TXT"
+            })
     else:
-        raise HTTPException(
-            status_code=400,
-            detail="Extensão de arquivo inválida. Os arquivos devem ser PDF ou TXT."
-        )
+        raise HTTPException(status_code=400, detail={
+            "ok": 1,
+            "message": "Extensão de arquivo inválida. Os arquivos devem ser PDF ou TXT"
+        })
 
     return await classify_email(email_content)
